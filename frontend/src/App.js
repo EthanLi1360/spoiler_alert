6fa96a18762d8863cba1deac9c599098fc1009f3
EthanLi15 <ethanli1360@gmail.com>
2024-10-01 15:45:40 -0400

    AI sandbox to generate recipes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react"
 
 function App() {
+  const [recipeText, setRecipeText] = React.useState("Recipe goes here!");
+
+  async function run() {
+    const {
+      GoogleGenerativeAI
+    } = require("@google/generative-ai");
+  
+    const apiKey = "ask Ethan for the api key";
+    const genAI = new GoogleGenerativeAI(apiKey);
+  
+    const model = genAI.getGenerativeModel({
+      model: "gemini-1.5-flash",
+    });
+  
+    const generationConfig = {
+      temperature: 1,
+      topP: 0.95,
+      topK: 64,
+      maxOutputTokens: 8192,
+      responseMimeType: "text/plain",
+    };
+
+    const chatSession = model.startChat({
+      generationConfig,
+      // safetySettings: Adjust safety settings
+      // See https://ai.google.dev/gemini-api/docs/safety-settings
+      history: [
+      ],
+    });
+
+    const result = await chatSession.sendMessage("Please give me a random recipe!");
+    console.log(result.response.text());
+    setRecipeText(result.response.text());
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>AI sandbox</h1>
+      <button onClick={run}>Click me!</button>
+      <p>{recipeText}</p>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +56,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
