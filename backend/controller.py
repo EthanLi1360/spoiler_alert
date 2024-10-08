ebb7cbd859e49ab2f92b32a37e99222abc360e41
tommynguyen <hieu.ntrung30@gmail.com>
2024-10-08 19:33:36 -0400

    changed routing

--- backend/controller.py
+++ backend/controller.py
@@ -7,12 +7,12 @@ from get_food import get_food
 
 app = Flask(__name__)
 
-@app.route('/fridge', methods=['POST'])
+@app.route('/fridges', methods=['POST'])
 def add_fridge():
     data = request.get_json()
     return create_fridge(data)
 
-@app.route('/fridge/<int:fridge_id>', methods=['DELETE'])
+@app.route('/fridges/<int:fridge_id>', methods=['DELETE'])
 def remove_fridge(fridge_id):
     return delete_fridge(fridge_id)
 
@@ -21,7 +21,7 @@ def user_fridges(user_id):
     fridges = get_user_fridges(user_id)
     return {"fridges": fridges}, 200
 
-@app.route('/fridge/<int:fridge_id>', methods=['GET'])
+@app.route('/fridges/<int:fridge_id>', methods=['GET'])
 def return_food(fridge_id):
     food = get_food(fridge_id)
     return jsonify(food), 200
