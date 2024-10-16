58e1f47e0ed129d0d78c8db13a6793b9b57bad49
John DeVoe <johndevoe2016@gmail.com>
2024-10-16 14:25:53 -0400

    Organized into more components and module based css

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -1,18 +1,12 @@
-import "./Home.css";
+import styles from "./Home.module.css";
+import Navbar from "./Navbar";
+import Title from "./Title";
 
 function Home(){
     return(
-        <div className="container">
-            <ul className="navbar">
-                <li style={{float: "left"}}><a href="/">SpoilerAlert</a></li>
-                <li><a href="/login">Login</a></li>
-                <li><a href="/login">Signup</a></li>
-            </ul>
-            <div className="title">
-                <h1>SPOILER ALERT</h1>
-                <h3>Smart Fridge, Smarter Recipes</h3>
-                <p>Track, Organize, and Cook with Ease</p>
-            </div>
+        <div className={styles.container}>
+            <Navbar />
+            <Title />
         </div>
     )
 }
