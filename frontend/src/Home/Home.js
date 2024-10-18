b652d4d92e0ba170b75d451824274077a072f552
EthanLi15 <ethanli1360@gmail.com>
2024-10-18 14:25:41 -0400

    Added features to homepage

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -1,12 +1,14 @@
 import styles from "./Home.module.css";
 import Navbar from "./Navbar";
 import Title from "./Title";
+import Features from "./Feature"
 
 function Home(){
     return(
         <div className={styles.container}>
             <Navbar />
             <Title />
+            <Features />
         </div>
     )
 }
