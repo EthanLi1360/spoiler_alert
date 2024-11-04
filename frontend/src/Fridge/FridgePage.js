52662e3377a5020da1440dfc7f6deabbf2a2d3fa
EthanLi15 <ethanli1360@gmail.com>
2024-11-03 22:12:48 -0500

    prep for merge to main

--- frontend/src/Fridge/FridgePage.js
+++ frontend/src/Fridge/FridgePage.js
@@ -5,11 +5,21 @@ import closedFridge from './closed_fridge.png';
 import openFridge from './open_fridge.png';
 
 const FridgePage = () => {
+  const [food, setFood] = useState({
+    CreateDate: Date.now(), // the created Date of the food.
+    name: "",
+    expirationDate: "",
+    quantity: "",
+  });
   const [foods, setFoods] = useState([]);
   const [name, setName] = useState('');
   const [expirationDate, setExpirationDate] = useState('');
   const [quantity, setQuantity] = useState('');
   const [isFridgeOpen, setIsFridgeOpen] = useState(false); // tracking if fridge is open or closed
+
+  // Searching functionalities
+  const [foodSearchedName, setfoodSearchedName] = useState("");
+  const [foodsSearched, setFoodsSearched] = useState([]);
 
   // Toggle fridge state
   const toggleFridge = () => {
@@ -18,21 +28,58 @@ const FridgePage = () => {
 
   // add food functionality
   const addFoodItem = () => {
-    const newItem = {
-      name,
-      expirationDate,
-      quantity,
-      id: Date.now() // unique id for each item
-    };
-    setFoods([...foods, newItem]);
-    setName('');
-    setExpirationDate('');
-    setQuantity('');
+    setFood({ CreateDate: Date.now(), ...food });
+    if (foods.length == 0) {
+      setFoods([...foods, food]);
+      alert(
+        `Successfully added ${food.name} of ${food.quantity}! ${
+          isNearExpiration ? "Near Expiration !" : ""
+        }`
+      );
+    }
+    for (let i = 0; i < foods.length; i++) {
+      if (food.name == foods[i].name) {
+        foods[i].quantity = Number(foods[i].quantity) + Number(food.quantity);
+        alert(
+          `You have already added ${food.name}, you now have ${foods[i].quantity} amount of ${food.name}`
+        );
+        break;
+      } else {
+        setFoods([...foods, food]);
+        alert(
+          `Successfully added ${food.name} of ${food.quantity}! ${
+            isNearExpiration ? "Near Expiration !" : ""
+          }`
+        );
+      }
+    }
   };
 
   // remove food item functionality
   const removeFoodItem = (id) => {
     setFoods(foods.filter(food => food.id !== id));
+  };
+
+  const removeFoodItemByCreateDate = () => {
+    setFoods(foods.filter((f) => f.CreateDate !== food.CreateDate));
+    setFoodsSearched(foodsSearched.filter((f) => f.name !== foodSearchedName));
+  };
+
+  const removeFoodItemByName = (name) => {
+    setFoods(foods.filter((food) => food.name == name));
+  };
+
+  const SearchFoodItem = () => {
+    let temp = foods.find((food) => food.name === foodSearchedName);
+    console.log(temp);
+    if (temp) {
+      setFoodsSearched([...foodsSearched, {
+        CreateDate: temp.CreateDate, // the created Date of the food.
+        name: temp.name,
+        expirationDate: temp.expirationDate,
+        quantity: temp.quantity,
+      }]);
+    }
   };
 
   // Check if the food is close to expiration (3 days or less)
@@ -76,26 +123,64 @@ const FridgePage = () => {
 
         {/* Show the form only when the fridge is open */}
         {isFridgeOpen && (
-          <div>
-            <input
-              type="text"
-              placeholder="Food Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <input
-              type="date"
-              value={expirationDate}
-              onChange={(e) => setExpirationDate(e.target.value)}
-            />
-            <input
-              type="number"
-              placeholder="Quantity (grams)"
-              value={quantity}
-              onChange={(e) => setQuantity(e.target.value)}
-            />
-            <button onClick={addFoodItem}>Add Food Item</button>
-          </div>
+          <>
+            <div>
+              <input
+                type="text"
+                placeholder="Food Name"
+                value={food.name}
+                onChange={(e) => setFood({ ...food, name: e.target.value })}
+              />
+              <input
+                type="date"
+                value={food.expirationDate}
+                onChange={(e) =>
+                  setFood({ ...food, expirationDate: e.target.value })
+                }
+              />
+              <input
+                type="number"
+                placeholder="Quantity (grams)"
+                value={food.quantity}
+                onChange={(e) => setFood({ ...food, quantity: e.target.value })}
+              />
+              <button onClick={addFoodItem}>Add Food Item</button>
+            </div>
+
+            <div className="modifyItems">
+              <h3>Search the name of an item to modify</h3>
+              <input
+                type="text"
+                onChange={(e) => setfoodSearchedName(e.target.value)}
+              />
+              <button onClick={SearchFoodItem}> Search</button>
+              {/*pop up the foodSearched*/}
+              <ul>
+                {foodsSearched.map((foodSearched) => (
+                  <li key={foodSearched.CreateDate} className="foodSearch">
+                    <p>Information of {foodSearched.name}</p>
+                    <span>
+                      {foodSearched.name} -- {foodSearched.quantity} grams
+                    </span>
+                    <span>
+                      {foodSearched.name} -- Created at {foodSearched.CreateDate}
+                    </span>
+                    <span>
+                      {foodSearched.name} -- Expires on:{" "}
+                      {foodSearched.expirationDate}
+                    </span>
+                    <button
+                      onClick={() =>
+                        removeFoodItemByCreateDate(foodSearched.CreateDate)
+                      }
+                    >
+                      Delete this food item
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </>
         )}
       </div>
     </div>
